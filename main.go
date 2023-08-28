14359c11bf0355aaa5850a8198954e15e2fde670
Júlio Agripino <juliocesar014@live.com>
2023-08-28 14:28:37 -0300

    feat: Create struct logs

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"github.com/juliocesar014/go-api/config"
 	"github.com/juliocesar014/go-api/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	// initializer config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize router
 	router.Initialize()
 }
